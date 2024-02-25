36abed285a39ddeab83a4e7953d2d110b472e8e2
Printer <88195101+imprimeriberac@users.noreply.github.com>
2024-02-25 02:58:38 +0100

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,7 @@
+import math
+
+factorielle_800 = math.factorial(800)
+print("La factorielle de 800 est :", factorielle_800)
+
+# Ajout d'une pause pour empêcher la fermeture automatique de la fenêtre
+input("Appuyez sur Entrée pour quitter...")
